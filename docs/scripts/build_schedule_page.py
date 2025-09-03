66b3b9be487e6d3c16fa2a5b5a78369748f5b9f2
niko.sarcevic <nikolina.sarcevic@gmail.com>
2025-09-03 14:40:02 -0400

    updated the schedule page

--- docs/scripts/build_schedule_page.py
+++ docs/scripts/build_schedule_page.py
@@ -135,7 +135,7 @@ All times are in EDT and the in-person location is the Center for Health Educati
 ---
 
 """
-    outpath = os.path.join("docs", "schedule.md")
+    outpath = os.path.join("docs", "pages", "schedule.md")
     with open(outpath, "w", encoding="utf-8") as f:
         f.write(md_header)
         f.write(html_schedule)
